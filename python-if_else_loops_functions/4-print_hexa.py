0c4d025df53c69b9a977f41dc4b028ee98e1c089
Metin100 <9455@holbertonstudents.com>
2024-09-04 15:49:05 +0400

    solve

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 for i in range(99):
-    if i % 16 >= 10:
-        for k in range(97,103):
-            if (k % 16) + 9 == i % 16:
-                c = chr(k)
-    else:
-        c = i % 16
-    print("{} = 0x{}{}".format(i,int(i / 16), c))
+    c = chr((i % 16) + 87) if i % 16 >= 10 else i % 16
+    print("{} = 0x{}{}".format(i, int(i / 16), c))
