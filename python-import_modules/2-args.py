a6af393c18f77ed9c9efe34cf7c01f9f51ac6c18
Metin100 <9455@holbertonstudents.com>
2024-09-06 11:17:39 +0400

    solve

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
         print("{} argument:".format(len(argv) - 1))
         print("{}: {}".format(1, argv[1]))
     else:
-        print("{} arguments.".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
