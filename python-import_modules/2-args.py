8018c063c3eb91ddfb0a42824a2c2ef4009b1522
Metin100 <9455@holbertonstudents.com>
2024-09-06 11:18:07 +0400

    solve

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ from sys import argv
 
 if __name__ == '__main__':
     if len(argv) == 1:
-        print("{} argument.".format(len(argv) - 1))
+        print("{} arguments.".format(len(argv) - 1))
     elif len(argv) == 2:
         print("{} argument:".format(len(argv) - 1))
         print("{}: {}".format(1, argv[1]))
