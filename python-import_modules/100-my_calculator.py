796a5605726617e7db279361cec6804493fb2db7
Metin100 <9455@holbertonstudents.com>
2024-09-07 11:22:16 +0400

    solve

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -8,18 +8,17 @@ if __name__ == "__main__":
         exit(1)
 
     if argv[2] == '+':
-        res = op.add(int(argv[1]),int(argv[3]))
-        print("{} {} {} = {}".format(argv[1],argv[2],argv[3],res))
+        res = op.add(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
     elif argv[2] == '-':
-        res = op.sub(int(argv[1]),int(argv[3]))
-        print("{} {} {} = {}".format(argv[1],argv[2],argv[3],res))
+        res = op.sub(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
     elif argv[2] == '*':
-        res = op.mul(int(argv[1]),int(argv[3]))
-        print("{} {} {} = {}".format(argv[1],argv[2],argv[3],res))
+        res = op.mul(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
     elif argv[2] == '/':
-        res = op.div(int(argv[1]),int(argv[3]))
-        print("{} {} {} = {}".format(argv[1],argv[2],argv[3],res))
+        res = op.div(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    
