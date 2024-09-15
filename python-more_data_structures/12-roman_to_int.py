fb791a18f4cde6ee115d557fb181f34f29574a2f
Metin100 <9455@holbertonstudents.com>
2024-09-15 13:08:34 +0400

    solve

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,9 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    
     sum = 0
     roman_numerals = {'I': 1, 'V': 5, 'X': 10,
                       'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    
     for i in range(len(roman_string)):
         if i > 0 and roman_numerals[roman_string[i]] >\
               roman_numerals[roman_string[i - 1]]:
@@ -16,4 +14,3 @@ def roman_to_int(roman_string):
             sum += roman_numerals[roman_string[i]]
 
     return sum
-
