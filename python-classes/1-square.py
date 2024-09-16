3be1a10dfb3423827721be198ce1d7b8e89fee4d
Metin100 <9455@holbertonstudents.com>
2024-09-16 16:35:16 +0400

    solve

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define Square Class For Equaling Size"""
+
+class Square:
+    """Class of square"""
+    def __init__(self, __size):
+        """Function that equals size
+        Args:
+            self: self of class
+            size: size of square
+        """
+        self.__size = __size
