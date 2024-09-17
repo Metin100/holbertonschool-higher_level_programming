0061ce111fccb3807e610e1119b66074e7790554
Metin100 <9455@holbertonstudents.com>
2024-09-17 13:58:14 +0400

    solve

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Define Square Class For Equaling Size"""
+"""Define a class Square with private instance attribute size."""
 
 
 class Square:
-     """Square class with private attribute size"""
-     def __init__(self, size=0) -> None:
+    """Square class with private attribute size"""
+    def __init__(self, size=0) -> None:
         """Initialize Square with size attribute"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
