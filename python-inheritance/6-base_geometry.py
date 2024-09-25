f5800fcda1e59d4cfc8eb15987aa64236912ea4a
Metin100 <9455@holbertonstudents.com>
2024-09-25 19:45:20 +0400

    solve

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write and empty class"""
+
+
+class BaseGeometry:
+    """Class of defining public method of area"""
+    def area(self):
+        """function raises exception"""
+        raise Exception('area() is not implemented')
