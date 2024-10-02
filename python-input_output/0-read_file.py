6a1d8dc90639763747cc9256bcdac90ff8a1e33f
Metin100 <9455@holbertonstudents.com>
2024-10-02 17:20:39 +0400

    solve

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""File"""
 def read_file(filename=""):
+    """Function of returning inside of file"""
     with open(filename, encoding = 'utf-8') as f:
         print(f.read())
