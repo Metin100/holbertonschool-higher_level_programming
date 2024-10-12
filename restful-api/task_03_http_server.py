081c80f3844ce2883aeca063792a554306f26813
Metin100 <9455@holbertonstudents.com>
2024-10-12 14:28:14 +0400

    solve task

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
 import http.server
 import json
 import socketserver
 
 
-class BasicServer(http.server.BaseHTTPRequestHandler):
-
+class NewHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -17,21 +15,26 @@ class BasicServer(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            data = {"name": "John", "age": 30, "city": "New York"}
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
             self.wfile.write(json.dumps(data).encode())
 
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'OK')
+            self.wfile.write(b"OK")
 
         else:
             self.send_response(404)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write('404 Not Found'.encode())
 
-with socketserver.TCPServer(("", 8000), BasicServer) as httpd:
+
+with socketserver.TCPServer(("", 8000), NewHandler) as httpd:
     print(f"Serving at port {8000}")
     httpd.serve_forever()
