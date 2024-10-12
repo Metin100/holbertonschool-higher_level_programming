29d4eb7bc934abe3c1b2c6bdc97f4deeb89f1f81
Metin100 <9455@holbertonstudents.com>
2024-10-12 14:32:43 +0400

    solve task

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import http.server
+import json
 
 
-class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+class BasicServer(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
+
         if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -14,8 +16,8 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            data = '{"name": "John", "age": 30, "city": "New York"}'
-            self.wfile.write(data.encode())
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -25,10 +27,10 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'404 Not Found')
-    
+            self.wfile.write(b'404 Error Found')
 
-if __name__ == "__main__":
-    server_address = ('', 8000)
-    httpd = http.server.HTTPServer(server_address, SimpleAPIHandler)
-    httpd.serve_forever()
+
+server = ('', 8000)
+httpd = http.server.HTTPServer(server, BasicServer)
+print('Hello')
+httpd.serve_forever()
