0dd25cc9f91eab5e7ad350fbd4ec474f8df20795
Metin100 <9455@holbertonstudents.com>
2024-10-12 14:27:47 +0400

    solve task

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -12,22 +12,25 @@ class BasicServer(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'Hello, this is a simple API!')
+
         elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode('utf-8'))
+            self.wfile.write(json.dumps(data).encode())
+
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'OK')
+
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'FAIL - Undefined endpoint returned incorrect content')
+            self.wfile.write('404 Not Found'.encode())
 
 with socketserver.TCPServer(("", 8000), BasicServer) as httpd:
     print(f"Serving at port {8000}")
