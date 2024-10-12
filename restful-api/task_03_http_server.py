d5a023475a27f7106a190a0eb2f640be86f25e5d
Metin100 <9455@holbertonstudents.com>
2024-10-12 14:24:43 +0400

    solve task

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import http.server
 import json
+import socketserver
 
 
 class basicServer(http.server.BaseHTTPRequestHandler):
@@ -29,7 +30,6 @@ class basicServer(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'FAIL - Undefined endpoint returned incorrect content')
 
-server = ('', 8000)
-httpd = http.server.HTTPServer(server, basicServer)
-print('Hello')
-httpd.serve_forever()
+with socketserver.TCPServer(("", 8000), basicServer) as httpd:
+    print(f"Serving at port {8000}")
+    httpd.serve_forever()
