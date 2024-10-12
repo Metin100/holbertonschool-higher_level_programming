d13ccea224d83b63795572af2a1f4e93dfa90818
Metin100 <9455@holbertonstudents.com>
2024-10-12 14:26:35 +0400

    solve task

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,8 +4,7 @@ import json
 import socketserver
 
 
-class basicServer(http.server.BaseHTTPRequestHandler):
-
+class BasicServer(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == '/':
@@ -15,13 +14,13 @@ class basicServer(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'OK')
         else:
@@ -30,6 +29,6 @@ class basicServer(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'FAIL - Undefined endpoint returned incorrect content')
 
-with socketserver.TCPServer(("", 8000), basicServer) as httpd:
+with socketserver.TCPServer(("", 8000), BasicServer) as httpd:
     print(f"Serving at port {8000}")
     httpd.serve_forever()
