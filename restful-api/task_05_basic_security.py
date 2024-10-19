debaf49709561f9793810385228c5f5ab13dfd89
Metin100 <9455@holbertonstudents.com>
2024-10-19 15:43:04 +0400

    solve task

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -26,16 +26,17 @@ users = {
 @auth.verify_password
 def verify_password(username, password):
     user = users.get(username)
+
     if user and \
         check_password_hash(user['password'], password):
         return user
-
+    
+    return None
 
 @auth.login_required
-@app.route('/basic-protected', methods = ['GET'])
+@app.route('/basic-protected', methods=['GET'])
 def basic():
     return 'Basic Auth: Access Granted'
-
 
 @app.route('/login', methods=["POST"])
 def login():
@@ -45,7 +46,44 @@ def login():
     user = verify_password(username, password)
 
     if user is None:
-        return jsonify({'error':'Couldnt find'})
+        return jsonify({'error': 'user does not find'}), 401
     
-    access_token = create_access_token(identity=user)
-    return jsonify({'access_token' : access_token})
+    access_token = create_access_token(identity=user["username"])
+    return jsonify({'token': access_token})
+
+@app.route('/jwt-protected', methods=['GET'])
+@jwt_required()
+def protected():
+    return "JWT Auth: Access Granted"
+
+@app.route('/admin-only', methods=['GET'])
+@jwt_required()
+def admin_only():
+    current_user = get_jwt_identity()
+    if current_user['role'] != 'admin':
+        return jsonify({'error': 'role is not admin'}), 403
+    return jsonify({'Admin Access: Granted'}), 200
+
+
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    return jsonify({"error": "Missing or invalid token"}), 401
+
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    return jsonify({"error": "Invalid token"}), 401
+
+@jwt.expired_token_loader
+def handle_expired_token_error(err):
+    return jsonify({"error": "Token has expired"}), 401
+
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+    return jsonify({"error": "Token has been revoked"}), 401
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    return jsonify({"error": "Fresh token required"}), 401
+
+if __name__ == '__main__':
+    app.run()
