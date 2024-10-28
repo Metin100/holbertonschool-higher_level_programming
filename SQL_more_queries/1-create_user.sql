05e3f58c9716ca8a2d88bfa9e6fa45cdef342cb2
Metin100 <9455@holbertonstudents.com>
2024-10-28 16:35:08 +0400

    add specify

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create new user and give all grants for individual
-CREATE USER 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
