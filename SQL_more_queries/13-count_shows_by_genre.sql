cb9d4abbe679047909cd7813b1932810b4f2cc5a
Metin100 <9455@holbertonstudents.com>
2024-10-29 20:24:10 +0400

    solve task

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,13 +1,14 @@
--- SQL code to show null statements
-SELECT 
-g.name AS genre, COUNT(m.genre_id) AS number_of_shows
-FROM 
-tv_show_genres AS m
-INNER JOIN 
-tv_genres as g 
-ON 
-m.genre_id=g.id
-GROUP BY 
-g.id
-ORDER BY 
-number_of_shows DESC
+-- A script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT
+    g.name AS genre,
+    COUNT(m.show_id) AS number_of_shows
+FROM
+    tv_genres AS g
+INNER JOIN
+    tv_show_genres AS m
+ON
+    g.id = m.genre_id
+GROUP BY
+    g.name
+ORDER BY
+    number_of_shows DESC;
