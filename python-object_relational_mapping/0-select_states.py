7f2c59a87bbf3f8cda5b225a2bbe4f41e5a4b4c3
Metin100 <9455@holbertonstudents.com>
2024-11-01 16:33:19 +0400

    solve task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,21 +8,21 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = argv[1],
-        passwd = argv[2],
-        db = argv[3]
-    )
-    mycursor = db.cursor()
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
     try:
-        mycursor.execute("""SELECT * FROM states ORDER BY id""")
-        data = mycursor.fetchall()
+        cur.execute("""SELECT * FROM states ORDER BY id""")
+        rows = cur.fetchall()
     except MySQLdb.Error as e:
         print(e)
 
-    for row in data:
+    for row in rows:
         print(row)
-    
-    mycursor.close()
+
+    cur.close()
     db.close()
