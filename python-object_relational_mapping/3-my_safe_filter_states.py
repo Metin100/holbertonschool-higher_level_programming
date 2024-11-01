c9e02e139010d10c3e871b621cdf9ea009a6db07
Metin100 <9455@holbertonstudents.com>
2024-11-01 18:02:46 +0400

    solve

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,13 +7,10 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
     )
     mycursor = db.cursor()
     try:
