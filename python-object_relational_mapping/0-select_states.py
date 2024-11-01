0654ba8204f6462c9c1949f1c6b7f809498c0eff
Metin100 <9455@holbertonstudents.com>
2024-11-01 16:43:20 +0400

    solve task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     )
     cursor = db.cursor()
 
-    cursor.execute('SELECT * FROM states ORDER BY id')
+    cursor.execute('SELECT * FROM states')
 
     for state in cursor.fetchall():
         print(state)
