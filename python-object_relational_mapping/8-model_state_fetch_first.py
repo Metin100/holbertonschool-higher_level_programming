fc4614f753fee44c0fb255051738535b14cc2843
Metin100 <9455@holbertonstudents.com>
2024-11-03 17:48:17 +0400

    solve task

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,11 +12,16 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]))
 
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
     first_state = session.query(State).order_by(State.id).first()
 
-    print("{}: {}".format(first_state.id, first_state.name))
+    if first_state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
 
     session.close()
