9776cbce16e28abd883c53262e3e15a524dcc525
Metin100 <9455@holbertonstudents.com>
2024-11-08 21:49:42 +0400

    solve

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/items')
 def items():
-    with open('items.json', 'r') as file:
-        data = load(file)
+    with open('items.json') as f:
+        data = load(f)
         items = data.get('items') or []
-    return render_template('templates/items.html', items=items)
+        
+    return render_template('items.html', items=items)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+app.run(port=5000, debug=True)
